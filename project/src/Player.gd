c2baf43358039026b3e74d99bbe59ee49939d098
Collin Stiner <collincollin16@gmail.com>
2020-11-02 21:34:53 -0500

    Fix player movement physics

--- project/src/Player.gd
+++ project/src/Player.gd
@@ -1,43 +1,36 @@
 extends KinematicBody2D
 
-
-const gravity = 200
-const speed_max = 300
-const acceleration = 500
-const jump = 275
-const friction = 0.5
-
-var direction := Vector2(0, 0)
-var linear_velocity = Vector2(0, 0)
+const RUN_SPEED :int = 150
+const JUMP_SPEED : int = -550
+const GRAVITY : int = 1200
+var velocity := Vector2()
+var jumping : bool = false
 
 onready var animated_sprite := $AnimatedSprite
 
+func get_input():
+	velocity.x = 0
+	var right = Input.is_action_pressed('ui_right')
+	var left = Input.is_action_pressed('ui_left')
+	var jump = Input.is_action_just_pressed('ui_select')
+
+	if jump and is_on_floor():
+		if !right and !left:
+			jumping = true
+			animated_sprite.play("jump")
+		velocity.y = JUMP_SPEED
+	if right:
+		animated_sprite.set_flip_h(false)
+		animated_sprite.play("walk")
+		velocity.x += RUN_SPEED
+	if left:
+		animated_sprite.set_flip_h(true)
+		animated_sprite.play("walk")
+		velocity.x -= RUN_SPEED
+	if !left and !right and !jumping:
+		animated_sprite.play("idle")
 
 func _physics_process(delta):
-	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	
-	animated_sprite.play("idle" if direction.x == 0 else "walk")
-	animated_sprite.scale.x = -1 if direction.x < 0 else 1
-	
-	if direction.x != 0:
-		linear_velocity.x += direction.x * acceleration * delta
-		linear_velocity.x = clamp(linear_velocity.x, -speed_max, speed_max)
-		
-	linear_velocity.y += gravity * delta
-	
-	if is_on_floor():
-		if direction.x == 0:
-			linear_velocity.x = lerp(linear_velocity.x, 0, friction)
-			
-		if Input.is_action_just_pressed("jump"):
-			linear_velocity.y = -jump 
-	else:
-		animated_sprite.play("jump")
-		
-		if Input.is_action_just_released("jump") and linear_velocity.y < -jump/2:
-			 linear_velocity.y = -jump/2
-			
-		if direction.x == 0:
-			linear_velocity.x = lerp(linear_velocity.x, 0, 0.02)
-			
-	var _ignored = move_and_slide(linear_velocity, Vector2.UP)
+	get_input()
+	velocity.y += GRAVITY * delta
+	velocity = move_and_slide(velocity, Vector2(0, -1))
