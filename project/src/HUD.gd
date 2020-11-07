08b90ca41ab569d594edd2c59747b3857e0a1e04
Collin Stiner <collincollin16@gmail.com>
2020-11-07 18:50:07 -0500

    Add pause menu

---
+++ project/src/HUD.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+func _process(delta):
+	if Input.is_action_just_pressed('pause'):
+		get_tree().paused = true
+		$PausePopup.show()
+	
+
+	
+
+
+
+func _on_ContinueButton_pressed():
+	$PausePopup.hide()
+	get_tree().paused = false
